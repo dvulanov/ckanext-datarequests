d59cf08937c5d9ee6f21969abd0451e11546d226
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2016-04-04 01:22:21 +0200

    Remove boolean validator

--- ckanext/datarequests/plugin.py
+++ ckanext/datarequests/plugin.py
@@ -113,10 +113,9 @@ class DataRequestsPlugin(p.SingletonPlugin):
     def update_config_schema(self, schema):
 
         ignore_missing = tk.get_validator('ignore_missing')
-        boolean_validator = tk.get_validator('boolean_validator')
 
         schema.update({
-            'ckan.datarequests.ignore_auth': [ignore_missing, boolean_validator],
+            'ckan.datarequests.ignore_auth': [ignore_missing],
         })
 
         return schema
